163829166cdc878144549dfea592c302b3ad1470
lasha tatulashvili <lashatatu@gmail.com>
2021-02-15 10:13:46 +0400

    21. link and detail page

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,7 +8,7 @@ const BlogNavbar = () => {
         expand="lg"
      >
        <Navbar.Brand className="fj-navbar-brand">
-         <a href="#">Filip-Jerga</a >
+         <a href="#">Lasha Tatulashvili</a >
        </Navbar.Brand >
        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
        <Navbar.Collapse id="basic-navbar-nav">
