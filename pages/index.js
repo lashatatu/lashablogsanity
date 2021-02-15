02791a04856e0e815a0f90819eeabd3db5980b6c
lasha tatulashvili <lashatatu@gmail.com>
2021-02-15 09:27:44 +0400

    16. date and image

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,9 @@ import AuthorIntro from 'components/AuthorIntro';
 import CardListItem from 'components/CardListItem';
 import CardItem from 'components/CardItem';
 
-import {getAllBlogs} from 'lib/api';
+import { getAllBlogs } from 'lib/api';
 
-export default function Home ( { blogs }) {
+export default function Home ( { blogs } ) {
   return (
      <PageLayout >
        <AuthorIntro />
@@ -16,28 +16,28 @@ export default function Home ( { blogs }) {
          {/*  <CardListItem />*/}
          {/*</Col >*/}
 
-         { blogs.map(blog=>
-            <Col key={blog.slug} md="4">
+         {blogs.map(blog =>
+            <Col
+               key={blog.slug}
+               md="4"
+            >
               <CardItem
-              title={blog.title}
-              subtitle={blog.subtitle}
+                 title={blog.title}
+                 subtitle={blog.subtitle}
               />
             </Col >
          )
-
-
-
          }
        </Row >
      </PageLayout >
   );
 }
 
-export async function getStaticProps(){
-  const blogs=await getAllBlogs()
-  return{
-    props:{
+export async function getStaticProps () {
+  const blogs = await getAllBlogs();
+  return {
+    props: {
       blogs
     }
-  }
+  };
 }
