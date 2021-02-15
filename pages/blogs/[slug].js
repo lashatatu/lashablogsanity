513a4a56647f54a67e0d74606b9d2dab2453faeb
lasha tatulashvili <lashatatu@gmail.com>
2021-02-15 11:28:11 +0400

    26. section introduction

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,11 +1,30 @@
 import PageLayout from '../../components/PageLayout';
-import {getBlogBySlug} from '../../lib/api';
+import {getAllBlogs, getBlogBySlug} from '../../lib/api';
+import {Col, Row} from 'react-bootstrap';
+import BlogHeader from '../../components/BlogHeader';
 
 const BlogDetail = ({blog}) => {
-
   return (
-     <PageLayout >
-       <h1 >hello detail page- {blog?.slug}</h1 >
+     <PageLayout className="blog-detail-page">
+       <Row >
+         <Col md={{span: 10, offset: 1}}>
+           <BlogHeader
+           title={blog.title}
+           subtitle={blog.subtitle}
+           coverImage={blog.coverImage}
+           author={blog.author}
+           date={blog.date}
+           />
+           <hr />
+           {/* Blog Content Here */}
+           It is a long established fact that a reader will be distracted by the readable content of a page when looking
+           at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,
+           as opposed to using 'Content here, content here', making it look like readable English. Many desktop
+           publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for
+           'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the
+           years, sometimes by accident, sometimes on purpose (injected humour and the like).
+         </Col >
+       </Row >
      </PageLayout >
   );
 };
@@ -17,20 +36,11 @@ export async function getStaticProps ({params}) {
   };
 }
 
-export function getStaticPaths () {
+export async function getStaticPaths () {
+  const blogs = await getAllBlogs();
   return {
-    paths: [
-      {
-        params: {slug:'my-first-blog-post'}
-      },
-      {
-        params: {slug:'my-second-blog-post'}
-      },
-      {
-        params: {slug:'my-third-blog-post'}
-      }
-    ],
-    fallback:false
+    paths: blogs?.map(b => ({params: {slug: b.slug}})),
+    fallback: false
   };
 }
 
