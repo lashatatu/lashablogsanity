0f9f4a450a54c840b7c2deae6e2ee5b909f9f26e
lasha tatulashvili <lashatatu@gmail.com>
2021-02-16 17:46:14 +0400

    46. section introduction

--- pages/index.js
+++ pages/index.js
@@ -1,83 +1,74 @@
-import {Col, Row} from 'react-bootstrap';
+import { useState } from 'react';
+
+import { Row, Col } from 'react-bootstrap';
 import PageLayout from 'components/PageLayout';
 import AuthorIntro from 'components/AuthorIntro';
-import CardListItem from 'components/CardListItem';
 import CardItem from 'components/CardItem';
-import FilteringMenu from '../components/FilteringMenu';
-import {useState} from 'react';
-import useSWR from 'swr';
+import CardListItem from 'components/CardListItem';
+import FilteringMenu from 'components/FilteringMenu';
 
-import {getAllBlogs} from 'lib/api';
-import {useGetBlogs, useGetHello} from '../actions';
+import { getAllBlogs } from 'lib/api';
+import { useGetBlogs } from 'actions';
 
-const fetcher = (url) => fetch(url).then(res => res.json());
-
-export default function Home ({blogs: initialData}) {
+export default function Home({blogs: initialData}) {
   const [filter, setFilter] = useState({
-    view: {list: 1}
+    view: { list: 0 }
   });
 
-  const {data:blogs, error} = useGetBlogs(initialData)
+  const { data: blogs, error } = useGetBlogs(initialData);
 
   return (
-     <PageLayout >
+     <PageLayout>
        <AuthorIntro />
        <FilteringMenu
           filter={filter}
           onChange={(option, value) => {
+            debugger
             setFilter({...filter, [option]: value});
           }}
        />
-       <hr />
-
+       <hr/>
        <Row className="mb-5">
-         {blogs.map(blog =>
+         { blogs.map(blog =>
             filter.view.list ?
-               <Col
-                  key={`${blog.slug}-list`}
-                  md={'9'}
-               >
+               <Col key={`${blog.slug}-list`} md="9">
                  <CardListItem
                     author={blog.author}
                     title={blog.title}
                     subtitle={blog.subtitle}
                     date={blog.date}
-                    slug={blog.slug}
                     link={{
-                      href: `/blogs/[slug]`,
+                      href: '/blogs/[slug]',
                       as: `/blogs/${blog.slug}`
                     }}
                  />
-               </Col >
+               </Col>
                :
-               <Col
-                  key={blog.slug}
-                  md="4"
-               >
+               <Col key={blog.slug} md="4">
                  <CardItem
                     author={blog.author}
                     title={blog.title}
                     subtitle={blog.subtitle}
                     date={blog.date}
                     image={blog.coverImage}
-                    slug={blog.slug}
                     link={{
-                      href: `/blogs/[slug]`,
+                      href: '/blogs/[slug]',
                       as: `/blogs/${blog.slug}`
                     }}
                  />
-               </Col >
-         )}
-       </Row >
-     </PageLayout >
-  );
+               </Col>
+         )
+         }
+       </Row>
+     </PageLayout>
+  )
 }
 
-export async function getStaticProps () {
-  const blogs = await getAllBlogs({offset:3});
+export async function getStaticProps() {
+  const blogs = await getAllBlogs({offset: 0});
   return {
     props: {
       blogs
     }
-  };
+  }
 }
