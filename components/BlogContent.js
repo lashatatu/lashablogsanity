53453189cf3f3f7937bb198027344fc1da701437
lasha tatulashvili <lashatatu@gmail.com>
2021-02-16 06:05:57 +0400

    32. image improvements

--- components/BlogContent.js
+++ components/BlogContent.js
@@ -1,23 +1,24 @@
 import BlockContent from '@sanity/block-content-to-react';
+import HighlightCode from './HighlightCode';
 
 const serializers = {
   types: {
     code: ({node: {language, code, filename}}) => {
       return (
-         <pre data-language={language}>
-          <code>{code}</code>
-          <p>{filename}</p>
-        </pre>
-      )
+         <HighlightCode language={language}>
+           {code}
+           <div className={'code-filename'}>{filename}</div >
+         </HighlightCode >
+      );
     }
   }
-}
+};
 
 const BlogContent = ({content}) =>
    <BlockContent
       imageOptions={{w: 320, h: 240, fit: 'max'}}
       serializers={serializers}
       blocks={content}
-   />
+   />;
 
 export default BlogContent;
