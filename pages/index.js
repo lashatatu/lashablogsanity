11799008d585144719af2faeed2fdf8d485f2b55
lasha tatulashvili <lashatatu@gmail.com>
2021-02-17 16:55:39 +0400

    55. blank image

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
-import {Button, Row} from 'react-bootstrap';
+import { Row, Button } from 'react-bootstrap';
 import PageLayout from 'components/PageLayout';
 import AuthorIntro from 'components/AuthorIntro';
 import FilteringMenu from 'components/FilteringMenu';
 
-import {useGetBlogsPages} from 'actions/pagination';
-import {getAllBlogs} from 'lib/api';
+import { useGetBlogsPages } from 'actions/pagination';
+import { getAllBlogs } from 'lib/api';
 
-export default function Home ({blogs}) {
+export default function Home({blogs}) {
   const [filter, setFilter] = useState({
-    view: {list: 0},
-    date: {asc:0}
+    view: { list: 0 },
+    date: { asc: 0 }
   });
 
   const {
@@ -22,7 +22,7 @@ export default function Home ({blogs}) {
   } = useGetBlogsPages({blogs, filter});
 
   return (
-     <PageLayout >
+     <PageLayout>
        <AuthorIntro />
        <FilteringMenu
           filter={filter}
@@ -30,30 +30,28 @@ export default function Home ({blogs}) {
              setFilter({...filter, [option]: value})
           }
        />
-       <hr />
+       <hr/>
        <Row className="mb-5">
          {pages}
-       </Row >
-       <div style={{textAlign:'center'}}>
+       </Row>
+       <div style={{textAlign: 'center'}}>
          <Button
             onClick={loadMore}
             disabled={isReachingEnd || isLoadingMore}
-            size={'lg'}
-            variant={'outline-secondary'}
-         >
-           {isLoadingMore ? '...' : isReachingEnd ?'no more blogs to load' : 'more blogs'}
-
-         </Button >
-       </div >
-     </PageLayout >
-  );
+            size="lg"
+            variant="outline-secondary">
+           {isLoadingMore ? '...' : isReachingEnd ? 'No more blogs' : 'More Blogs'}
+         </Button>
+       </div>
+     </PageLayout>
+  )
 }
 
-export async function getStaticProps () {
+export async function getStaticProps() {
   const blogs = await getAllBlogs({offset: 0, date: 'desc'});
   return {
     props: {
       blogs
     }
-  };
+  }
 }
