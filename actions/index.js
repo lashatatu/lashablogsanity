d400518148cd3bf7c3e7d3fadfa958c7745da840
lasha tatulashvili <lashatatu@gmail.com>
2021-02-17 16:07:27 +0400

    50. fetch more blogs

--- actions/index.js
+++ actions/index.js
@@ -1,11 +1,16 @@
 import useSWR from 'swr';
 
-const fetcher = (url) => fetch(url).then(res => res.json());
+const fetcher = url => fetch(url).then(res => res.json())
 
-export const useGetHello=()=>{
-  return useSWR('/api/hello', fetcher);
-}
+export const useGetHello = () => useSWR('/api/hello', fetcher);
 
-export const useGetBlogs=(initialData)=>{
-  return useSWR(`/api/blogs`, fetcher,{initialData})
+export const getBlogs = (url) => {
+  return fetcher(url)};
+
+export const useGetBlogs = ({offset, filter}, initialData) => {
+
+  return useSWR(`
+    /api/blogs?offset=${offset || 0}&date=${filter.date.asc ? 'asc' : 'desc'}`,
+     fetcher,
+     { initialData })
 }
--- actions/pagination.js
+++ actions/pagination.js
@@ -1,64 +1,29 @@
-import {useSWRInfinite} from 'swr';
-import {useGetBlogs} from 'actions';
-import {Col} from 'react-bootstrap';
-import CardItem from 'components/CardItem';
-import CardListItem from 'components/CardListItem';
 
-export const useGetBlogsPages = ({blogs: initialData, filter}) => {
+import { useSWRInfinite } from 'swr';
+import { getBlogs } from 'actions';
 
-  return useSWRInfinite(
-     'index-page',
-     ({offset, withSWR}) => {
-
-       const {data: blogs} = withSWR(useGetBlogs(initialData));
-
-       if ( !blogs ) {
-         return 'Loading...';
+export const useGetBlogsPages = ({filter}) => {
+  const result = useSWRInfinite(
+     (index, previousPageData) => {
+       if (index === 0 ) {
+         return `/api/blogs?date=${filter.date.asc ? 'asc' : 'desc'}`
        }
 
-       return blogs
-          .map(blog =>
-             filter.view.list ?
-                <Col
-                   key={`${blog.slug}-list`}
-                   md="9"
-                >
-                  <CardListItem
-                     author={blog.author}
-                     title={blog.title}
-                     subtitle={blog.subtitle}
-                     date={blog.date}
-                     link={{
-                       href: '/blogs/[slug]',
-                       as: `/blogs/${blog.slug}`
-                     }}
-                  />
-                </Col >
-                :
-                <Col
-                   key={blog.slug}
-                   md="4"
-                >
-                  <CardItem
-                     author={blog.author}
-                     title={blog.title}
-                     subtitle={blog.subtitle}
-                     date={blog.date}
-                     image={blog.coverImage}
-                     link={{
-                       href: '/blogs/[slug]',
-                       as: `/blogs/${blog.slug}`
-                     }}
-                  />
-                </Col >
-          );
+       if (!previousPageData.length) {
+         return null
+       }
+
+       return `/api/blogs?offset=${index * 6}&date=${filter.date.asc ? 'asc' : 'desc'}`
      },
-     // here you will compute offset that will get passed into previous callback function with 'withSWR'
-     // SWR: data you will get from 'withSWR' function
-     // index: number of current page
-     (SWR, index) => {
-       return 0;
-     },
-     [filter]
-  );
-};
+     getBlogs,
+  )
+
+  let hitEnd = false;
+  const { data } = result;
+
+  if (data) {
+    hitEnd = data[data.length - 1].length === 0
+  }
+
+  return {...result, hitEnd}
+}
