16f57c0eb00cacdac41310b528a4e5d13fa4ec2a
lasha tatulashvili <lashatatu@gmail.com>
2021-02-19 05:46:58 +0400

    61. apply themes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,34 +1,38 @@
-import { Nav, Navbar } from 'react-bootstrap';
+import {Nav, Navbar} from 'react-bootstrap';
 import Link from 'next/link';
+import {useTheme} from '../providers/ThemeProvider';
 
 const BlogNavbar = () => {
-  return (
-     <Navbar
-        className="fj-navbar fj-nav-base"
-        bg="transparent"
-        expand="lg"
-     >
-       <Navbar.Brand className="fj-navbar-brand">
-         <Link href={'/'}>
-           <a >Lasha Tatulashvili</a >
-         </Link >
-       </Navbar.Brand >
-       <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-       <Navbar.Collapse id="basic-navbar-nav">
-         <Nav className="ml-auto">
-           <Nav.Link
-              as={() =>
-                 <Link href={'/'}>
-                   <a className="fj-navbar-item fj-navbar-link">Home</a >
-                 </Link >
-              }
-              href="/"
-           >Home
-           </Nav.Link >
-         </Nav >
-       </Navbar.Collapse >
-     </Navbar >
-  );
+	const {theme, toggleTheme} = useTheme();
+	return (
+		 <Navbar
+				className="fj-navbar fj-nav-base"
+				bg="transparent"
+				expand="lg"
+		 >
+			 <Navbar.Brand className="fj-navbar-brand">
+				 <Link href={'/'}>
+					 <a >Lasha Tatulashvili</a >
+				 </Link >
+			 </Navbar.Brand >
+			 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+			 <Navbar.Collapse id="basic-navbar-nav">
+				 <Nav className="ml-auto">
+					 <Nav.Link
+							as={() =>
+								 <Link href={'/'}>
+									 <a className="fj-navbar-item fj-navbar-link">Home</a >
+								 </Link >
+							}
+							href="/"
+					 />
+						 <button className="btn btn-success" onClick={toggleTheme}>
+							 {theme.type}
+						 </button>
+				 </Nav >
+			 </Navbar.Collapse >
+		 </Navbar >
+	);
 };
 
 export default BlogNavbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,20 @@
-import {library, config} from '@fortawesome/fontawesome-svg-core';
+import ThemeProvider from 'providers/ThemeProvider';
+import { library, config } from '@fortawesome/fontawesome-svg-core';
 import {
   faBorderAll,
   faList,
   faSortNumericDown,
   faSortNumericUp } from '@fortawesome/free-solid-svg-icons';
 
-config.autoAddCss=false;
+config.autoAddCss = false;
 library.add(faList, faBorderAll, faSortNumericDown, faSortNumericUp);
 
-import '@fortawesome/fontawesome-svg-core/styles.css'
-import 'styles/globals.css';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'highlight.js/styles/dracula.css'
-import 'styles/index.scss'
+import 'highlight.js/styles/darcula.css';
+import 'styles/index.scss';
 
-
-function MyApp ( { Component, pageProps } ) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
+export default ({Component, pageProps}) =>
+   <ThemeProvider>
+     <Component {...pageProps} />
+   </ThemeProvider>
