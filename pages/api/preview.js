88b7effaba24bb757f233fbe3357058765eb3a1d
lasha tatulashvili <lashatatu@gmail.com>
2021-02-19 11:47:40 +0400

    65. get preview to page

---
+++ pages/api/preview.js
@@ -0,0 +1,18 @@
+import { getBlogBySlug } from 'lib/api';
+
+export default async function enablePreview(req, res) {
+
+	if (req.query.secret !== process.env.SANITY_PREVIEW_SECRET || !req.query.slug) {
+		return res.status(401).json({message: 'Invalid token'})
+	}
+
+	const blog = await getBlogBySlug(req.query.slug);
+
+	if (!blog) {
+		return res.status(401).json({message: 'Invalid Slug1111!'})
+	}
+
+	res.setPreviewData({message: 'Hello World'});
+	res.writeHead(307, { Location: `/blogs/${blog.slug}`})
+	res.end();
+}
